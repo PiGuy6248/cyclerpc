53b7657d37e1491f97ca895457e4f6aec43f5f8d
PiGuy <45721243+PiGuy6248@users.noreply.github.com>
2019-04-02 18:41:54 -0500

    update for cycle compatibility

--- rpc.js
+++ rpc.js
@@ -1,7 +1,17 @@
 const discord = require('discord-rich-presence')('CLIENT_ID_HERE');
 let starttime = Date.now();
+let details = ["DETAILS", "MORE DETAILS", "ETC"];
 console.log("Started!")
 discord.updatePresence({
+        details: details[Math.round(Math.random() * (details.length - 1))],
+        startTimestamp: starttime,
+        largeImageKey: "KEY_HERE",
+        smallImageKey: "KEY_HERE",
+        largeImageText: "TEXT HERE",
+        smallImageText: "TEXT HERE",
+        instance: true
+})        
+setInterval(() => discord.updatePresence({
         details: details[Math.round(Math.random() * (details.length - 1))],
         startTimestamp: starttime,
         largeImageKey: "KEY_HERE",
